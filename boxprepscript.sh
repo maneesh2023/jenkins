a28d0fd49fa91c92d9b1e48dd1b5443200fc4199
maneesh2023 <123784681+maneesh2023@users.noreply.github.com>
2023-01-28 16:50:26 -0600

    Update boxprepscript.sh

--- boxprepscript.sh
+++ boxprepscript.sh
@@ -3,10 +3,11 @@
 sudo apt-get update
 sudo apt install -y git net-tools docker.io
 sudo snap install docker
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo rm -rf ~/myprojects
 sudo mkdir ~/myprojects
 cd ~/myprojects
 sudo git clone https://github.com/maneesh2023/jenkins.git
 cd ~/myprojects/jenkins
+sudo docker-compose down
 sudo docker-compose pull
 sudo docker-compose up
