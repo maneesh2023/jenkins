7301cf3e71123c2df13ecf7b8498260641afa3e5
maneesh2023 <123784681+maneesh2023@users.noreply.github.com>
2023-01-28 09:49:28 -0600

    Update 000-aks-deploy.groovy

--- jenkinsfiles/000-aks-deploy.groovy
+++ jenkinsfiles/000-aks-deploy.groovy
@@ -17,17 +17,12 @@ pipeline {
         }
         stage('Deploy to AKS') {
             steps {
-                withCredentials([string(credentialsId: 'AZURE_CLIENT_ID', variable: 'AZURE_CLIENT_ID'),
-                                 string(credentialsId: 'AZURE_CLIENT_SECRET', variable: 'AZURE_CLIENT_SECRET'),
-                                 string(credentialsId: 'AZURE_TENANT_ID', variable: 'AZURE_TENANT_ID'),
-                                 string(credentialsId: 'AZURE_SUBSCRIPTION_ID', variable: 'AZURE_SUBSCRIPTION_ID')]) {
                     sh '''
                         az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
                         az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --overwrite-existing --admin
                         kubectl create namespace $AKS_NAMESPACE
                         kubectl apply -f azure-vote.yaml -n $AKS_NAMESPACE
                     '''
-                }
             }
         }
     }
