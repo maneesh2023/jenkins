4cde3175ae0fb783e3c2707b2e1fea15570314c6
maneesh2023 <123784681+maneesh2023@users.noreply.github.com>
2023-01-28 17:07:33 -0600

    Update boxprepscript.sh

--- boxprepscript.sh
+++ boxprepscript.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 sudo apt install -y git net-tools docker.io
 sudo snap install docker
-sudo rm -rf ~/myprojects
-sudo mkdir ~/myprojects
+sudo rm -rf ~/github
+sudo mkdir ~/github
 cd ~/myprojects
 sudo git clone https://github.com/maneesh2023/jenkins.git
 cd ~/myprojects/jenkins
